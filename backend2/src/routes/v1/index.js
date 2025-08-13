ecc313be4ba5a9b535790e24033ddd7de12a23d0
Aryan Kashyap <aryankashyapbanka2003@gmail.com>
2025-08-13 11:51:38 +0530

    Improved get project api using user id

--- backend2/src/routes/v1/index.js
+++ backend2/src/routes/v1/index.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import userRouter from './users.js'
+import userRouter from './users.js';
+import projectRouter from './projects.js'
  
  const router = express.Router();
 
  router.use('/users', userRouter);
+ router.use('/projects', projectRouter);
+
  
 
  
